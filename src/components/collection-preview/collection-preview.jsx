85b6ba7946365ba8b240a602314bb645815134dc
Denisse Abreu <denisseabreu21@yahoo.com>
2022-02-04 20:34:40 -0400

    create shop page and collections

---
+++ src/components/collection-preview/collection-preview.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import CollectionItem from "../collection-item/collection-item";
+
+import '../collection-preview/collection-preview.scss';
+
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+      .filter((_item, indx) => indx < 4)
+      .map(({id, ...otherItemProps}) => (
+        <CollectionItem key={id} {...otherItemProps} />
+      ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
