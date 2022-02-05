239c9c2ff49a2fe8872b21d5836f2a1828830df7
Denisse Abreu <denisseabreu21@yahoo.com>
2022-02-04 20:33:23 -0400

    import app.css into app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import {HomePage} from './pages/homepage/homepage';
+import ShopPage from './pages/shop/shop';
 
-import './pages/homepage/homepage.scss';
+import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 function App() {
   return (
@@ -16,7 +12,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     </BrowserRouter>
