0a4ec3447380cd5dc6477d53078cbd33925114eb
Denisse Abreu <denisseabreu21@yahoo.com>
2022-02-08 21:29:25 -0400

    adding firebase and google signin

--- src/App.js
+++ src/App.js
@@ -4,23 +4,48 @@ import {HomePage} from './pages/homepage/homepage';
 import SignInAndUp from './pages/sign-in-and-up/sign-in-and-up';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
+import {auth} from './firebase/firebase.utils';
 
 import './App.css';
 
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndUp} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Header currentUser={this.state.currentUser} />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInAndUp} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
