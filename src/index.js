1c9a9a57f440be44ff987fc03aa61ac422315b6c
Denisse Abreu <denisseabreu21@yahoo.com>
2022-02-22 21:37:52 -0400

    adding redux-persist

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
+
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+import './index.css';
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
 
+//<React.StrictMode> StrictMode is a tool for highlighting potential problems in an application.
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.unregister();
